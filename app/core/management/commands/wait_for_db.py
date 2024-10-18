c24f76f370d698426e6848de481684853220b70e
priyanshu251 <iampriyanshu251@gmail.com>
2024-10-18 19:05:57 +0530

    custom wait_for_db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,10 +1,23 @@
 """command for django app to wait for db to get ready"""
 
+import time
 from django.core.management.base import BaseCommand
 # This line imports the BaseCommand class from Django's core.management.base module. BaseCommand is a base class for creating custom management commands in Django. By inheriting from BaseCommand, you can define your own commands that can be run using Django's manage.py script.
+from psycopg2 import OperationalError as Psycopg2OpError  # this op error is raised by postgresql when db is not ready as psycopg2 is the db adapter for postgresql
+from django.db.utils import OperationalError  # this op error is raised by django when db is not ready
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     # This line defines a new class named Command that inherits from BaseCommand. The name Command is significant because Django looks for a class with this name when it searches for custom management commands. By inheriting from BaseCommand, the Command class gains the ability to be executed as a management command.
     
     def handle(self, *args, **options): # This line defines a handle() method on the Command class. This method is called when the management command is executed. The handle() method takes two arguments: *args and **options. The *args argument is a list of positional arguments, and the **options argument is a dictionary of keyword arguments.
-        pass
+        self.stdout.write('Waiting for database...')    
+        db_up=False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up=True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))    
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,7 +2,7 @@
 
 from unittest.mock import patch # This line imports the patch function from the unittest.mock module. The patch function is used to temporarily replace the target object with a mock object during the test.
 
-from psycopg2 import OperationalError as psycopg2Error # This line imports the OperationalError exception from the psycopg2 module and renames it to psycopg2Error. This exception is used to handle database-related errors specific to PostgreSQL.
+from psycopg2 import OperationalError as Psycopg2OpError # This line imports the OperationalError exception from the psycopg2 module and renames it to psycopg2Error. This exception is used to handle database-related errors specific to PostgreSQL.
 
 from django.core.management import call_command # This line imports the call_command function from django.core.management. The call_command function is used to programmatically call a Django management command.
 
@@ -17,15 +17,15 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True  # sets the return value of the patched_check mock object to True. This means that whenever the check method is called during the test, it will return True instead of executing its actual implementation. This is done to simulate the behavior of the check method without actually running it. 
         # hardcoding the value to True in the actual implementation would be problematic because it would not accurately reflect the real state of the database. The purpose of setting patched_check.return_value = True in the test is to simulate a scenario where the database is ready, allowing you to test the behavior of the wait_for_db command under controlled conditions.
         call_command('wait_for_db')  # This line calls the wait_for_db management command using the call_command function. This is the command being tested.
-        patched_check.assert_called_once(database=['default'])  # The line patched_check.assert_called_once_with(database=['default']) is an assertion in the test that verifies the behavior of the wait_for_db management command. Passing database as an argument.
+        patched_check.assert_called_once_with(databases=['default'])  # The line patched_check.assert_called_once_with(database=['default']) is an assertion in the test that verifies the behavior of the wait_for_db management command. Passing database as an argument.
 
     # this function is used to test the wait_for_db command when the database is not ready. since db is not ready we will get an operational error
     # we will apply delay
     @patch('time.sleep')  # patch will ben applied on this function only
     def test_wait_for_db_delay(self,patched_sleep,patched_check):  # order of the arguments must be same as the order of the decorators
-        patched_check.side_effect = [psycopg2Error]*2 + [OperationalError]*3 + [True]  # returning true at 6th time hoping db will be ready by then
+        patched_check.side_effect = [Psycopg2OpError]*2 + [OperationalError]*3 + [True]  # returning true at 6th time hoping db will be ready by then
         # explain
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count,6)
-        patch.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
 
