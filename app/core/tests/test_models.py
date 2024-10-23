897f384784e5fe8328a989de7a9c10b576ecb03b
priyanshu251 <iampriyanshu251@gmail.com>
2024-10-23 20:35:57 +0530

    created custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,7 +10,7 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-        # a function in django used to retrieve user model currently active in this project. If there is no custom user model defined, it will return the default user model provided by Django(django.contrib.auth.models.User)
+        # get_user_model()--a function in django used to retrieve user model currently active in this project. If there is no custom user model defined, it will return the default user model provided by Django(django.contrib.auth.models.User)
         self.assertEqual(user.email, email)   # user here is the object of the user model
         self.assertTrue(user.check_password(password))  # we are using check_password method to check the password because the password is hashed and stored in the database. The check_password method provided by Django hashes the input password and compares it with the stored password hash to verify the password.
 
@@ -25,3 +25,14 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user=get_user_model().objects.create_user(email, 'test123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """"Test creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff) 
